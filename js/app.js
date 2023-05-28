dfc0989891c44f1cb957b4c3f46a41bf0feae55d
claudiomonzoni <claudiomonzoni@hotmail.com>
2023-05-27 22:22:12 -0600

    listo

--- js/app.js
+++ js/app.js
@@ -1,82 +1,99 @@
-
 // importo gsap
-import gsap from 'gsap'
+import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
-let tl = gsap.timeline({repeat:0})
+let tl = gsap.timeline({ repeat: 0 });
 
-tl.from ("img",{
-  stagger:0.1,
-  y:-200,
-  opacity: 0,
-  duration: 1 
-}, "-=.5")
-
-gsap.from ("input, select, #contactoBtn",{
-  scrollTrigger: {
-    trigger: "input, select",
+tl.from(
+  "img",
+  {
+    stagger: 0.1,
+    y: -200,
+    opacity: 0,
+    duration: 1,
   },
-  stagger:0.1,
-  y:-80,
-  opacity: 0,
-  duration: 1 
-}, "=.6")
+  "-=.5"
+);
 
-gsap.from ("nav, ul, li",{
-  scrollTrigger: {
-    trigger: "nav",
+gsap.from(
+  "input, select, #contactoBtn",
+  {
+    scrollTrigger: {
+      trigger: "input, select",
+    },
+    stagger: 0.1,
+    y: -80,
+    opacity: 0,
+    duration: 1,
   },
-  stagger:0.1,
-  y:80,
-  opacity: 0,
-  duration: 1 
-}, "-=.8")
+  "=.6"
+);
 
-gsap.from ("#social",{
-  scrollTrigger: {
-    trigger: "#social",
+gsap.from(
+  "nav, ul, li",
+  {
+    scrollTrigger: {
+      trigger: "nav",
+    },
+    stagger: 0.1,
+    y: 80,
+    opacity: 0,
+    duration: 1,
   },
-  stagger:0.1,
-  y:80,
-  opacity: 0,
-  duration: 1 
-}, "-=.5")
+  "-=.8"
+);
 
+gsap.from(
+  "#social",
+  {
+    scrollTrigger: {
+      trigger: "#social",
+    },
+    stagger: 0.1,
+    y: 80,
+    opacity: 0,
+    duration: 1,
+  },
+  "-=.5"
+);
 
-
-  window.$ = require('jquery');
+window.$ = require("jquery");
 
 $(document).ready(function () {
-
-$("#contactoBtn").click(function (e) {
-
-  var nombre = $("#nombre").val();
-    var apellido = $("#apellido").val();
-    var email = $("#email").val();
-    var tel = $("#tel").val();
-    var curso = $("#cursos").val();
+  $("#contactoBtn").click(function (e) {
+    var nombre = $("#nombre").val();
+    var fecha = $("#fecha").val();
+    var celular = $("#celular").val();
     var edo = $("#edo").val();
-    var ciudad = $("#ciudad").val();
+    var curso = $("#curso").val();
+    var promos  = $("#promos").val();
 
     $("#msj").empty(); // To empty previous error/success message.
     // Checking for blank fields.
 
-    if (nombre == "" || apellido == "" || curso == ""|| tel == "" ||  email == "") {
-      alert("Por favor llena los campos marcados con * al final");
+    if (
+      nombre == "" ||
+      fecha == "" ||
+      celular == "" ||
+      curso == "")
+       {
+      alert("Por favor llena todos los campos");
     } else {
-      
-
       $.post(
         "contacto.php",
         {
-          nombre1: nombre, apellido1: apellido, email1: email, tel1: tel,  curso1: curso, edo1: edo, ciudad1: ciudad
+          nombre1: nombre,
+          fecha1: fecha,
+          celular1: celular,
+          edo1: edo,
+          curso1: curso,
+          promos1: promos,
         },
         function (data) {
           $("#msj").append(data); // Append returned message to message paragraph.
           if (
-            data ==
-            "Gracias, pronto un asesor se pondra en contacto contigo 😎"
+            data == "Gracias por tu registro, pronto un asesor se pondra en contacto contigo 😎"
           ) {
             $("#formu").reset(); // To reset form fields on success.
           }
@@ -86,8 +103,7 @@ $("#contactoBtn").click(function (e) {
 
     e.preventDefault();
   });
-
-})
+});
 // cargar modulos solo por secciones
 // switch (document.location.pathname)
 // {
